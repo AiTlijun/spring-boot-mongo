f9f27aa40c212105fef36460ca4315884de02a3e
AiTlijun <fenfenniu0519>
2018-09-11 22:50:29 +0800

    rabbitmq

---
+++ src/main/java/com/example/springbootmongo/config/Rabbit/Receiver/HelloReceiver.java
@@ -0,0 +1,17 @@
+package com.example.springbootmongo.config.Rabbit.Receiver;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(queues = "hello")
+public class HelloReceiver {
+
+    @RabbitHandler
+    public void process(String hello) {
+        System.out.println("Receiver  : " + hello);
+    }
+
+
+}
