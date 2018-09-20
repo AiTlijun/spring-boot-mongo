648f4d34dcb8ae51896ed2872cb8f9c5755f1a2c
lijun <976991218@qq.com>
2018-09-20 15:02:17 +0800

    mybatis gennerator

---
+++ src/main/java/com/example/springbootmongo/entity/UserLog.java
@@ -0,0 +1,45 @@
+package com.example.springbootmongo.entity;
+
+import java.util.Date;
+
+public class UserLog {
+    private Integer id;
+
+    private String username;
+
+    private String operation;
+
+    private Date operationdate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation == null ? null : operation.trim();
+    }
+
+    public Date getOperationdate() {
+        return operationdate;
+    }
+
+    public void setOperationdate(Date operationdate) {
+        this.operationdate = operationdate;
+    }
+}
---
+++ src/main/java/com/example/springbootmongo/mapper/UserLogMapper.java
@@ -0,0 +1,17 @@
+package com.example.springbootmongo.mapper;
+
+import com.example.springbootmongo.entity.UserLog;
+
+public interface UserLogMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(UserLog record);
+
+    int insertSelective(UserLog record);
+
+    UserLog selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(UserLog record);
+
+    int updateByPrimaryKey(UserLog record);
+}
